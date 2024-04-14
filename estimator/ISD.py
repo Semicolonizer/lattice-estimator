ac3eca66963ac355f39cdb64b9ae67f6954d7218
Semicolonizer <cricke_96@hotmail.com>
2024-04-14 22:20:02 +0300

    fix calculation using N as number of codewords of certain weight

--- estimator/ISD.py
+++ estimator/ISD.py
@@ -2,7 +2,7 @@ from math import log, comb
 
 class ISDCost:
     @staticmethod
-    def lee_brickell(q, n, k, w, L=1):
+    def lee_brickell(q, n, k, w, N_w=0, L=1):
         """
         Estimate the cost of the Lee-Brickell algorithm for ISD. [?:LIP-UPKE]
 
@@ -12,22 +12,27 @@ class ISDCost:
         k (int): The dimension of the code.
         w (int): The weight of the codeword.
         L (int): The number of codewords to find.
+        N_w (int): The cardinality of 
         
         Returns:
         cost (float): The estimated cost.
         """
 
         #Approximateed expected cardinality of A_w
-        N_w = comb(n, w) * ((q-1) ** (w-2) * q ** (k-n+1))
+        if N_w == 0:
+            N_w = comb(n, w) * ((q-1) ** (w-2) * q ** (k-n+1))
+
+        # Number of codewords of weight w
+        N = (q-1)*N_w
 
         #Complexity for C_inf
         C_inf = (q * comb(n, w)) / comb(n-k,w-2)
 
         #Estimated complexity
-        if L == N_w:
+        if L == N:
             cost = C_inf * log(N_w)
         else:
-            cost = (L / N_w) * C_inf
+            cost = (L / N) * C_inf
         
-        return log(cost,2)
+        return cost
 
