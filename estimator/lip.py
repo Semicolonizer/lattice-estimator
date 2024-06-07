21a4d994f4f5c3e1693301543a43118da4d188ff
Semicolonizer <cricke_96@hotmail.com>
2024-06-07 14:34:21 +0300

    add LIP __call__ functionality

--- estimator/lip.py
+++ estimator/lip.py
@@ -39,21 +39,38 @@ class Estimate:
 
         return res
     
-    def __call__(self, lip_parameters):
+    def __call__(self, params):
         """
         Provides a full estimation for the Lattice Isomorphism Problem when the class instance is called.
 
         Parameters:
-        lip_parameters (dict): A dictionary of parameters necessary for the precise estimation.
+        params (dict): A dictionary of parameters necessary for the precise estimation.
 
         Returns:
         res (dict): The cost estimation results for attacks.
         """
 
         algorithms = {}
+        algorithms['hull'] = hullattack
+        
+        res_raw = batch_estimate(
+            params, algorithms.values(), log_level=1, jobs=jobs, catch_exceptions=catch_exceptions
+        )
+        res_raw = res_raw[params]
+        res = {
+            algorithm: v
+            for algorithm, attack in algorithms.items()
+            for k, v in res_raw.items()
+            if f_name(attack) == k
+        }
 
-        # Compute estimations based on the parameters
-        res = 0  # TODO: Replace with actual logic
+        for algorithm in algorithms:
+            if algorithm not in res:
+                continue
+            result = res[algorithm]
+            if result["rop"] != oo:
+                print(f"{algorithm:8s} :: {result!r}")
+
         return res
 
 estimate = Estimate()
