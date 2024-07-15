c40b7a1957e6afe4166d4b7945c168d892d1d872
Semicolonizer <cricke_96@hotmail.com>
2024-07-16 00:11:04 +0300

    add attacks to lip interface

--- estimator/lip.py
+++ estimator/lip.py
@@ -1,5 +1,6 @@
 from .lip_parameters import LIPParameters as Parameters
 from .lip_hull import hullattack
+from .lip_havreg import havreg
 from .util import batch_estimate, f_name
 from sage.all import oo
 
@@ -14,10 +15,19 @@ class Estimate:
 
         Returns:
         res (dict): The rough cost estimation results.
+
+        
+        EXAMPLES:
+        
+        >>> from estimator import *
+        >>> _ = LIP.estimate.rough(LIP.Parameters(200,127))
+        hull     :: rop: ≈2^86.7, tag: ssa, red: ≈2^86.7, δ: 1.006187, β: log(200) + 200.0, d: 400, non_red: ≈2^23.0
+        havreg   :: rop: ≈2^132.9, red: ≈2^132.9, δ: 1.003982, β: 400, d: 400
         """
 
         algorithms = {}
         algorithms['hull'] = hullattack
+        algorithms['havreg'] = havreg 
         
         res_raw = batch_estimate(
             params, algorithms.values(), log_level=1, jobs=jobs, catch_exceptions=catch_exceptions
@@ -39,7 +49,7 @@ class Estimate:
 
         return res
     
-    def __call__(self, params):
+    def __call__(self, params, jobs=1, catch_exceptions=True):
         """
         Provides a full estimation for the Lattice Isomorphism Problem when the class instance is called.
 
@@ -48,10 +58,18 @@ class Estimate:
 
         Returns:
         res (dict): The cost estimation results for attacks.
+
+        EXAMPLES:
+
+        >>> _ = LIP.estimate(LIP.Parameters(300,127))
+        hull     :: rop: ≈2^115.0, tag: ssa, red: ≈2^115.0, δ: 1.004779, β: log(300) + 300.0, d: 600, non_red: ≈2^24.7
+        havreg   :: rop: ≈2^188.6, red: ≈2^188.6, δ: 1.002986, β: 600, d: 600
+
         """
 
         algorithms = {}
         algorithms['hull'] = hullattack
+        algorithms['havreg'] = havreg 
         
         res_raw = batch_estimate(
             params, algorithms.values(), log_level=1, jobs=jobs, catch_exceptions=catch_exceptions
